7753cecfdb468302fc9d1c2b909b3f1553a53c78
nishita24011998 <nishita.cairsc.mathura@gmail.com>
2022-09-07 01:23:44 +0530

    add springrest

--- springrest/src/main/java/com/springrest/springrest/controller/MyController.java
+++ springrest/src/main/java/com/springrest/springrest/controller/MyController.java
@@ -1,13 +1,22 @@
 package com.springrest.springrest.controller;
 
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.springrest.springrest.entities.Course;
 
 @RestController
 public class MyController {
 	@GetMapping("/home")
 	public String home() {
 		return "Welcome to courses Application";
+	}
+	
+	@GetMapping("/courses")
+	public List<Course> getCourses() {
+	
 	}
  
 }
---
+++ springrest/src/main/java/com/springrest/springrest/services/CourseService.java
@@ -0,0 +1,9 @@
+package com.springrest.springrest.services;
+
+import java.util.List;
+package com.springrest.springrest.services;
+
+public interface CourseService {
+	public List<course> getCourses();
+
+}
